059f24046c9b7cb863ce66a210b3c40188014573
Manish10022001 <manishshirsat2850@gmail.com>
2026-02-28 13:25:41 +0530

    Remove node_modules from source control

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { RouterProvider } from "react-router-dom";
+import 'react-toastify/dist/ReactToastify.css';
+import Aos from 'aos';
+import { QueryClient, QueryClientProvider, } from '@tanstack/react-query'
+import router from './routes/router.jsx'
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+  //   <RouterProvider router={router} />,
+  // </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router}/>
+  </QueryClientProvider>
 )
