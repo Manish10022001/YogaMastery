6834f803fb6386653b9994f36f5725520217628f
Manish10022001 <manishshirsat2850@gmail.com>
2026-03-05 12:52:03 +0530

    Navbar enhancement

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router'
+import Navbar from '../components/headers/Navbar'
 
 const MainLayout = () => {
   return (
-    <main className='dark:bg-black overflow-hidden'>
-        <nav>Navbar</nav>
+    <main className='dark:bg-white overflow-hidden'>
+        <Navbar />
         {/* need to give outlet so get the children routes / path */}
         <Outlet />
         <footer>Footer</footer>
